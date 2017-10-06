fe4dae9b0372416b4fd4ec9a26e12f8d4f861e5c
ydhwang <31387866+youngdonhwang@users.noreply.github.com>
2017-10-07 02:06:09 +0900

    enum addon

---
+++ java1/src/java1/Season.java
@@ -0,0 +1,5 @@
+package java1;
+
+public enum Season {
+	SPRING, SUMMER, FALL, WINTER
+}
--- java1/src/java1/mainjava.java
+++ java1/src/java1/mainjava.java
@@ -4,7 +4,7 @@ public class mainjava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, Java");
+		System.out.println("Hello, Java in in the GitHub");
 
 	}
 
