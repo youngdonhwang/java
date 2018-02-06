9c0ac4d34833a03e7f7404a4725f253c3c1ec1a2
youngdonhwang <stmhyd@naver.com>
2018-02-06 10:19:07 +0900

    local pc modified

--- java1/src/java1/mainjava.java
+++ java1/src/java1/mainjava.java
@@ -5,6 +5,7 @@ public class mainjava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Season season;
-		System.out.println("Hello, Java1 in in the GitHub of " + Season.SUMMER);
+		System.out.println("Hello, Java11 in in the GitHub of " + Season.SUMMER);
+		System.out.println("Hello, Java12 in in the GitHub of " + Season.SUMMER);
 	}
 }
